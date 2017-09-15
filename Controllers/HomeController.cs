db377aafecef47d49c28aee80ce9a79c80bd0aab
bozniac <nino.bosnic@yahoo.com>
2017-09-15 10:49:08 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using ContosoUniversity.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            var query = _context.Students
+                .GroupBy(s => s.EnrollmentDate)
+                .Select(g =>
+                    new EnrollmentDateGroup()
+                    {
+                        EnrollmentDate = g.Key,
+                        StudentCount = g.Count()
+                    }
+                );
+
+            return View(await query.AsNoTracking().ToListAsync());
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using ContosoUniversity.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoUniversity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    logger.LogDebug("DbInitializer.Initialize");
+                    DbInitializer.Initialize(context);
+                }
+                catch (System.Exception ex)
+                {
+                    logger.LogError(ex, "An error occured while seeding the database.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
